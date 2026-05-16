fce25cad9085b42686e505755dcac0824d184407
Mansour Emami <mansuurem@gmail.com>
2026-05-16 16:19:59 +0200

    order-service . entity finished first phase

---
+++ order-service/order-domain/order-domain-core/src/main/java/info/emami/order/service/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package info.emami.order.service.domain.entity;
+
+import info.emami.domain.entity.AggregateRoot;
+import info.emami.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+
+}
